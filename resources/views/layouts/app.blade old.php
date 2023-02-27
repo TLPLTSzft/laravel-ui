4de045e772d36af033a284e5e1e3245241ce1af5
TLPLTSzft <tlpltszft@gmail.com>
2023-02-27 07:25:05 +0100

    navigáció plusz

--- resources/views/layouts/app.blade old.php
+++ resources/views/layouts/app.blade old.php
@@ -52,7 +52,7 @@
                     </ul>
 
                     <!-- Right Side Of Navbar -->
-                    {{-- <ul class="navbar-nav ms-auto">
+                    <ul class="navbar-nav ms-auto">
                         <!-- Authentication Links -->
                         @guest
                             @if (Route::has('login'))
@@ -86,7 +86,7 @@
                                 </div>
                             </li>
                         @endguest
-                    </ul> --}}
+                    </ul>
                 </div>
             </div>
         </nav>
